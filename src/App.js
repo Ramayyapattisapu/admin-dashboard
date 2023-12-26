69c1a9930091b36b1ec66a01e4acf40c12aec042
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2023-12-26 17:11:44 +0530

    react sidebar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./app.css";
 
 function App() {
   return (
   <div>
     <Topbar/>
-    test <br/>
+    <div className="container">
+      <Sidebar/>
+    </div>
   </div>
   );
 }
