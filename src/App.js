10c10118aaa9044a9ee3327f703964bcf3fa775b
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2024-02-17 20:21:55 +0530

    routes modifies and data added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
@@ -19,6 +22,11 @@ function App() {
           <Route path="/users" element={<UserList />} />
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newuser" element={<NewUser />} />
+
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newproduct" element={<NewProduct />} />
         </Routes>
       </div>
     </Router>
--- src/dummyData.js
+++ src/dummyData.js
@@ -51,6 +51,28 @@ export const userData = [
     },
   ];
 
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 2000,
+    },
+    {
+      name: "Apr",
+      "Sales": 2780,
+    },
+  ];
+
+
+
   export const userRows = [
     { id: 1, username: "Mercury", avatar: profilePic, email:"mercury@gmail.com", status: "active", transaction: "$12.00" },
     { id: 2, username: "Venus", avatar: profilePic, email:"venus@gmail.com", status: "active", transaction: "$12.00" },
@@ -62,4 +84,16 @@ export const userData = [
     { id: 8, username: "Neptune", avatar: profilePic, email:"neptune@gmail.com", status: "active", transaction: "$12.00" },
     { id: 9, username: "Pluto", avatar: profilePic, email:"pluto@gmail.com", status: "active", transaction: "$12.00" },
   
+  ];
+
+  export const productRows = [
+    { id: 1, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 2, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 3, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 4, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 5, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 6, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 7, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 8, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
+    { id: 9, name: "Apple Airpods", img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?", stock:100, status: "active", price: "$123.00" },
   ];
